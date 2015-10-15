8c0c6271b3c0093a36a4243fc6f032f60b289678
rikatze <rikatze@email.com>
2015-10-15 15:16:16 +0900

    Fix usage.

--- Main.hs
+++ Main.hs
@@ -31,7 +31,7 @@ main :: IO ()
 main = do
   args <- getArgs
   if length args /= 2 then
-    putStrLn "usage: $ /path/to/shizuku (assembly.S) (outputPath)"
+    putStrLn "usage: $ /path/to/suiteki (assembly.S) (outputPath)"
   else
     writeBinary args
 
