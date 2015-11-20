ba64f8a057c4cf0a542aa4eef8319ddb4b9cc795
rikatze <rikatze@email.com>
2015-11-20 23:53:27 +0900

    Initial commit

---
+++ test/lat.ml
@@ -0,0 +1,11 @@
+let a1 = read_int () in
+let a2 = read_int () in
+let a3 = read_int () in
+let b1 = read_int () in
+let b2 = read_int () in
+let c1 = read_int () in
+let c2 = read_int () in
+let c3 = read_int () in
+let d1 = read_int () in
+print_int(a1 + a2 + a3 + b1 + b2 + c1 + c2 + c3 + d1)
+
